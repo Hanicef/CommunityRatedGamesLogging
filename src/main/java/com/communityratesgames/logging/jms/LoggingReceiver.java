7ad6eef3942c5dc71ba99a9fad11d01d2c2384f9
Hanicef <gustaf.alhall@yahoo.com>
2018-12-07 14:46:03 +0100

    Add logging receiver

--- src/main/java/com/communityratesgames/logging/jms/LoggingReceiver.java
+++ src/main/java/com/communityratesgames/logging/jms/LoggingReceiver.java
@@ -1,23 +1,30 @@
 package com.communityratesgames.logging.jms;
 
+import com.communityratesgames.logging.transactions.LoggingDataMethods;
+import com.communityratesgames.logging.domain.Logging;
+
 import org.apache.log4j.Logger;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
+import javax.ejb.*;
+import javax.inject.*;
+import javax.jms.*;
+import java.sql.Timestamp;
 
-@MessageDriven(activationConfig = {@ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jms/queue/crg")})
+@MessageDriven
 public class LoggingReceiver implements MessageListener {
 
     private final static Logger logger = Logger.getLogger(com.communityratesgames.logging.jms.LoggingReceiver.class);
 
+    @Inject
+    public LoggingDataMethods logMethods;
+
 /* Instead of REST end point, this will be the "endpoint" for a jms queue. Works like an websocket listener*/
     @Override
     public void onMessage(Message message) {
-        logger.info("ONMESSAGE IN RECEIVER: " + message);
         try {
+            Logging log = new Logging(0L, new Timestamp(System.currentTimeMillis()), ((TextMessage)message).getText());
+            logger.info("Got log: " + log.getMessage());
+            logMethods.addNewLog(log);
             System.out.println(message.getBody(String.class));
         } catch (JMSException ex) {
             logger.error("Reciever:  " + ex);
